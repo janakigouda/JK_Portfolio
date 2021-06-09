423e99ae9574f01fa025b3dd04b07da244fe8a9b
Janaki Kangalgouda <42041306+janakigouda@users.noreply.github.com>
2021-06-09 10:03:48 +0530

    Update timeline.jsx

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -21,7 +21,7 @@ export default class Timeline extends Component {
                         <i className="icon-pen2" />
                       </div>
                       <div className="timeline-label">
-                        <h2>Internship at ADIS Technologies <span>2020-present</span></h2>
+                        <h2>Internship at ADIS Technologies <span>Oct 2020 - May 2021</span></h2>
                         <p>I have joined the ADIS Technologies as a Web Developer-Intern in the React js team. My major part of the work has been into the field of developing user interface (Continuous Integration & Continuous Development) .</p>
                       </div>
                     </div>
